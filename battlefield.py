c099a1474f55b51403126f759d4a3f5622f4e35b
Deshawn Brown <brownd726@gmail.com>
2022-04-04 10:35:06 -0500

    added Battlefield class

---
+++ battlefield.py
@@ -0,0 +1,19 @@
+from robot import Robot
+from dinosuar import Dinosaur
+class Battlefield:
+
+    def __init__(self):
+        self.robot = Robot
+        self.dinosuar = Dinosaur
+
+    def run_game(self):
+        pass
+
+    def display_welcome(self):
+        pass
+
+    def battle_phase(self):
+        pass
+
+    def display_winner(self):
+        pass
